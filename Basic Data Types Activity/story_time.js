4fc2b43f2ed7e7dae18519d26e686c6353825fbc
Anthony <anth0nybec3rra13@gmail.com>
2024-06-18 10:49:45 -0700

    new file

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,30 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greeting = "Welcome to my Jungle!";
+let name = 'Kala';
+let animal = 'Panther';
+let role = 'Queen of the Jungle';
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+
+let age = 23
+let year = 1299
+let century = 13
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+
+let animalsAround = ['monkey','bear','snake','rabbit'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+
+let hasKids = false;
+
+// TODO: Print your story to the console.
+console.log(greeting + "My name is " + name + ".");
+console.log("I'm a " + animal + ".");
+console.log("I'm the " + role + ".");
+console.log("You have traveled back in time to " + year + ". The " + century + "th century.");
+console.log("Do any of you humans have a question?");
+console.log("Me: Yes do you have any kids?");
+console.log("Kala: " + hasKids + "\n");
+console.log("You guys are free to roam around, just be careful of " + animalsAround[2] + " he tends to be unforgiving to people he doesn't know.");
